d6ccecb9f438504f1fb2559b12fc338001d1af51
yuckey1123 <97144643+yuckey1123@users.noreply.github.com>
2022-09-20 02:14:59 +0900

    add: 4.トップページUI作成

---
+++ lib/model/user.dart
@@ -0,0 +1,13 @@
+class User {
+  String name;
+  String uid;
+  String? imagePath;
+  String lastMessage;
+
+  User({
+    required this.name,
+    required this.uid,
+    this.imagePath,
+    this.lastMessage = ''
+  });
+}
---
+++ lib/pages/top_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import '../model/user.dart';
+
+class TopPage extends StatefulWidget {
+  const TopPage({Key? key}) : super(key: key);
+
+  @override
+  State<TopPage> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  List<User> userList = [
+    User(
+        name: 'チェンバー',
+        uid: 'abc',
+        imagePath:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmJjWmOaEIRhHJTXoPs9OF-kzsdbS7OTbGew&usqp=CAU',
+        lastMessage: '消えてもらおうか'),
+    User(
+        name: 'セージ',
+        uid: 'def',
+        imagePath:
+            'https://1.bp.blogspot.com/-mwFpzCFijK8/X5pUCfETFEI/AAAAAAAADfQ/V5ShKUPV3B8XvYKUbf6nkWWaVymfPEd2QCLcBGAsYHQ/s2000/sage-is-getting-hit-by-the-nerf-hammer-in-valorant-patch-1-04-1.jpg',
+        lastMessage: 'まだ終わりじゃないわ'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('チャットアプリ'),
+        ),
+        body: ListView.builder(
+            itemCount: userList.length,
+            itemBuilder: (context, index) {
+              return SizedBox(
+                height: 70,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: userList[index].imagePath == null
+                            ? null
+                            : NetworkImage(userList[index].imagePath!),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          userList[index].name,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          userList[index].lastMessage,
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }));
+  }
+}
